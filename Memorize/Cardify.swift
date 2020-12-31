53af548957452512e0b9c65481a3fa62c952c1cb
Giang Nguyen <gn02.nguyen@gmail.com>
2020-12-31 12:37:22 -0800

    Animated flipping cards.

--- Memorize/Cardify.swift
+++ Memorize/Cardify.swift
@@ -7,8 +7,22 @@
 
 import SwiftUI
 
-struct Cardify: ViewModifier {
-    var isFaceUp: Bool
+struct Cardify: AnimatableModifier {
+    var rotation: Double
+
+    var isFaceUp: Bool {
+        rotation < 90
+    }
+    
+    init(isFaceUp: Bool) {
+        rotation = isFaceUp ? 0 : 180
+    }
+    
+    var animatableData: Double {
+        get {return rotation}
+        set {rotation = newValue}
+    }
+    
     func body(content: Content) -> some View {
         ZStack {
             if isFaceUp {
@@ -19,6 +33,7 @@ struct Cardify: ViewModifier {
                 RoundedRectangle(cornerRadius: cornerRadius).fill()
             }
         }
+        .rotation3DEffect(Angle.degrees(rotation), axis: (0, 1, 0))
     }
     private let cornerRadius: CGFloat = 10
     private let lineWidth: CGFloat = 3
