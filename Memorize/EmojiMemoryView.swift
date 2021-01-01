c0cb3cbd92e243238a7d2ffb7670cbd0c084cb73
Giang Nguyen <gn02.nguyen@gmail.com>
2020-12-31 16:43:44 -0800

    Calculated and showed bonus time on cards.

--- Memorize/EmojiMemoryView.swift
+++ Memorize/EmojiMemoryView.swift
@@ -30,13 +30,26 @@ struct EmojiMemoryView: View {
 }
 
 struct CardView: View {
+    @State var animatedBonusRemaining: Double = 0
+    private func startBonusTimeAnimation() {
+        animatedBonusRemaining = card.bonusRemaining
+        withAnimation(.linear(duration: card.bonusTimeRemaining)) {animatedBonusRemaining = 0}
+    }
     var card: MemoryGame<String>.Card
     var body: some View {
         GeometryReader { geometry in
             if card.isFaceUp || !card.isMatched {
                 ZStack() {
-                    Pie(startAngle: Angle.degrees(-90), endAngle: Angle.degrees(60), clockwise: true)
-                        .padding().opacity(0.4)
+                    Group {
+                        if card.isConsumingBonusTime {
+                            Pie(startAngle: Angle.degrees(-90), endAngle: Angle.degrees(-animatedBonusRemaining * 360 - 90), clockwise: true).onAppear{
+                                    self.startBonusTimeAnimation()
+                                }
+                        } else {
+                            Pie(startAngle: Angle.degrees(-90), endAngle: Angle.degrees(-card.bonusRemaining * 360 - 90), clockwise: true)
+                        }
+                    }.opacity(0.4).padding()
+
                     Text(card.content).rotationEffect(Angle.degrees(card.isMatched ? 360 : 0)).animation(card.isMatched ? Animation.linear(duration: 1).repeatForever(autoreverses: false) : .default)
                     }
                 .font(Font.system(size: fontSize(size: geometry.size)))
